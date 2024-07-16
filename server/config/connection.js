02e4a71cb4bfacded98479bf83066f951359c3db
Gayle McClure <gaylekdennison@gmail.com>
2024-07-16 21:58:22 +1000

    fixed connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://gaylekdennison:oAHQ9KUTcA17hJBj@cluster0.k4rv7ti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/google-books");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://gaylekdennison:oAHQ9KUTcA17hJBj@cluster0.k4rv7ti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/google-books');
 module.exports = mongoose.connection;
